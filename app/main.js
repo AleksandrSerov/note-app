79b091769f8568ec7217e9fe3422a2e7c74a037c
Aleksandr Serov <sashaserov2010@gmail.com>
2019-05-02 19:32:04 +0300

    Add functionality

--- app/main.js
+++ app/main.js
@@ -1,10 +1,19 @@
 const inputComponent = {
-  template: `<input  :placeholder="placeholder" v-model="input" type="text" class="input is-small"/>`,
+  template: `<input  :placeholder="placeholder" @keyup.enter="monitorEnterKey" v-model="input" type="text" class="input is-small"/>`,
   props: ["placeholder"],
   data() {
     return {
       input: ""
     };
+  },
+  methods: {
+    monitorEnterKey() {
+      this.$emit("add-note", {
+        note: this.input,
+        timestamp: new Date().toLocaleString()
+      });
+      this.input = "";
+    }
   }
 };
 
@@ -17,5 +26,11 @@ new Vue({
   },
   components: {
     "input-component": inputComponent
+  },
+  methods: {
+    addNote(event) {
+      this.notes.push(event.note);
+      this.timestamps.push(event.timestamp);
+    }
   }
 });
